a7ae79d148eac59464627a36380622c72eb4c14f
Devil666face <artem1999k@gmail.com>
2022-10-14 22:43:29 +0300

    first commit

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from replacer import WordReplacer
 
 
 if __name__=='__main__':
-    argv = SysArgv(sys.argv[1:])
-    replace = WordReplacer(argv)
+    # print(sys.argv[1:])
+    # with open('out.txt','w') as file:
+    #     for arg in sys.argv[1:]:
+    #         file.write(arg)
+    if sys.argv[1:][0]=='-h':
+        print('1:Path to directory with database and exe script\n2:id template\n3:Full path to new file')
+    else:
+        argv = SysArgv(sys.argv[1:])
+        replace = WordReplacer(argv)
